aad9df6421ed3acbdd9bba3a46124d4db2466544
rdunckel <rdunckel@L1171838606.wctc.msft>
2012-03-06 18:11:00 -0600

    initial commit

---
+++ src/lab2/Startup.java
@@ -0,0 +1,35 @@
+package lab2;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * See if you can modify this class to use Spring as in Lab1.
+ */
+public class Startup {
+
+    public static void main(String[] args) {
+        // First create some low-level instances using
+        // Liskov Substitution Principle
+
+        //TipCalculator calc1 = new FoodServiceTipCalculator(TipService.ServiceQuality.FAIR, 125.00);
+        //TipCalculator calc2 = new BaggageServiceTipCalculator(TipService.ServiceQuality.GOOD, 5);
+
+        // Next, create the high-level class
+        //TipService tipMgr = new TipService(calc1);
+
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TipService tipMgr = (TipService) ctx.getBean("tipService");
+
+        // Now ask the tip manager to do some work...
+        double tip = tipMgr.getTip();
+        String quality = tipMgr.getServiceQuality();
+
+        // Now prove it works...
+        System.out.println("\nThe tip for the "
+                + tipMgr.getTipCalculator().getClass().getSimpleName() + " is: "
+                + tip + ", with a service qualilty of: " + quality);
+
+    }
+}
